3972a8ebb3dd742e27737cf82f5e286a4a44d05e
pastillazz <robertoalonsopelayo26@gmail.com>
2026-09-15 19:22:17 -0400

    v-4.1

--- Infrastructure/Persistence/AppWriteDbContext.cs
+++ Infrastructure/Persistence/AppWriteDbContext.cs
@@ -1,29 +1,26 @@
-﻿
-using Domain.Entities;
+﻿using Domain.Entities;
 using Domain.Primitives;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
 
-public class AppWriteDbContext(DbContextOptions<AppWriteDbContext> options):
-    DbContext(options)
+public class AppWriteDbContext(DbContextOptions<AppWriteDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users => Set<User>();
     public DbSet<Company> Companies => Set<Company>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Ignore<IDomainEvent>();
-        modelBuilder
-            .ApplyConfigurationsFromAssembly(
-                typeof(AppWriteDbContext).Assembly,
-                WriteConfigurationFilter);
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppWriteDbContext).Assembly,
+            WriteConfigurationFilter
+        );
     }
-    
+
     private static bool WriteConfigurationFilter(Type type)
     {
         return type.Namespace?.Contains("Configurations.Write") ?? false;
     }
-    
 }
-
